0c60e00b8196cfa687baa247a9e6e036fbc63606
Martin Heidegger <martin.heidegger@gmail.com>
2018-12-05 13:06:00 +0900

    Added close/open/destroyed support to index and tests to verify that the events work.

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function RASPauseWrapper (ras) {
   }
   PauseWrapper.call(this, ras)
 
-  this.closed = ras.closed
+  this.opened = false
+  this.closed = false
+  this.destroyed = false
   this.readable = ras.readable
   this.writeable = ras.writeable
   this.deletable = ras.deletable
@@ -21,26 +23,43 @@ inherits(RASPauseWrapper, PauseWrapper)
 
 Object.assign(RASPauseWrapper.prototype, {
   open: function (cb) {
-    this._onResumeCb(cb, function (cb2) { this._proxied.open(cb2) })
+    var scope = this
+    this._onResumeCb(cb, function (cbProxy) {
+      this._proxied.open(function (err) {
+        if (err) return cbProxy(err)
+        scope.opened = true
+        cbProxy()
+      })
+    })
   },
   read: function (offset, size, cb) {
-    this._onResumeCb(cb, function (cb2) { this._proxied.read(offset, size, cb2) })
+    this._onResumeCb(cb, function (cbProxy) { this._proxied.read(offset, size, cbProxy) })
   },
   write: function (offset, buffer, cb) {
-    this._onResumeCb(cb, function (cb2) { this._proxied.write(offset, buffer, cb2) })
+    this._onResumeCb(cb, function (cbProxy) { this._proxied.write(offset, buffer, cbProxy) })
   },
   del: function (offset, size, cb) {
-    this._onResumeCb(cb, function (cb2) { this._proxied.del(offset, size, cb2) })
+    this._onResumeCb(cb, function (cbProxy) { this._proxied.del(offset, size, cbProxy) })
   },
   stat: function (cb) {
-    this._onResumeCb(cb, function (cb2) { this._proxied.stat(cb2) })
+    this._onResumeCb(cb, function (cbProxy) { this._proxied.stat(cbProxy) })
   },
   close: function (cb) {
     this._closed = true
-    return this._proxied.close(cb)
+    var scope = this
+    return this._proxied.close(function (err) {
+      if (err) return cb(err)
+      scope.closed = true
+      cb()
+    })
   },
   destroy: function (cb) {
     this._closed = true
-    return this._proxied.destroy(cb)
+    var scope = this
+    return this._proxied.destroy(function (err) {
+      if (err) return cb(err)
+      scope.destroyed = true
+      cb()
+    })
   }
 })
--- index.test.js
+++ index.test.js
@@ -72,3 +72,108 @@ tape('buffer constructor', function (t) {
     t.end()
   })
 })
+
+tape('pause after read', function (t) {
+  const mem = ram(Buffer.from('contents'))
+  const file = pauseWrap(mem)
+  const stack = []
+  const _read = mem.read
+  mem.read = function (offset, size, cb) {
+    stack.push('read() - internal')
+    _read.call(mem, offset, size, cb)
+  }
+
+  t.equals(file.pauseable, true)
+  t.equals(file.paused, false)
+  stack.push('.paused = true')
+  stack.push('read()#1')
+  file.read(0, 7, function readCb1 (err, buf) {
+    stack.push('readCb1()')
+    t.error(err)
+    t.deepEqual(buf, Buffer.from('content'))
+  })
+  file.paused = true
+  stack.push('read()#2')
+  file.read(0, 7, function readCb (err, buf) {
+    stack.push('readCb2()')
+    t.error(err)
+    t.deepEqual(buf, Buffer.from('content'))
+    t.deepEqual(stack, [
+      '.paused = true',
+      'read()#1',
+      'read() - internal',
+      'read()#2',
+      'setTimeout()',
+      '.paused = false',
+      'read() - internal',
+      'readCb1()',
+      'readCb2()'
+    ])
+    t.end()
+  })
+  stack.push('setTimeout()')
+  setTimeout(function () {
+    stack.push('.paused = false')
+    file.paused = false
+  }, 10)
+})
+
+tape('open/close/destroy', function (t) {
+  const mem = ram()
+  const file = pauseWrap(mem)
+  testState(false, false, false)
+  file.open(function (err) {
+    t.error(err, 'open successful')
+    testState(true, false, false)
+    file.close(function (err) {
+      t.error(err, 'close successful')
+      testState(true, true, false)
+      file.destroy(function (err) {
+        t.error(err, 'destroy successful')
+        testState(true, true, true)
+        t.end()
+      })
+    })
+  })
+  testState(false, false, false)
+  function testState (opened, closed, destroyed) {
+    t.equals(file.opened, opened, 'file.opened == ' + opened)
+    t.equals(file.closed, closed, 'file.closed == ' + closed)
+    t.equals(file.destroyed, destroyed, 'file.destroyed == ' + destroyed)
+    t.equals(mem.opened, opened, 'mem.opened == ' + opened)
+    t.equals(mem.closed, closed, 'mem.closed == ' + closed)
+    t.equals(mem.destroyed, destroyed, 'mem.destroyed == ' + destroyed)
+  }
+})
+
+tape('open/close while pause', function (t) {
+  const ram = pauseRam()
+  const stack = []
+  stack.push('pause()')
+  ram.pause()
+  stack.push('open()')
+  ram.open(function openCb () {
+    stack.push('openCb()')
+    stack.push('pause()')
+    ram.pause()
+    stack.push('close()')
+    ram.close(function () {
+      t.equals(ram.closed, true, 'now it is closed')
+      t.equals(ram.paused, true, 'close works while pause')
+      t.deepEqual(stack, [
+        'pause()',
+        'open()',
+        'resume()',
+        'openCb()',
+        'pause()',
+        'close()'
+      ])
+      t.end()
+    })
+  })
+  setTimeout(function () {
+    t.equals(ram.opened, false)
+    stack.push('resume()')
+    ram.resume()
+  }, 10)
+})
